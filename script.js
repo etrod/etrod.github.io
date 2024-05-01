f004b20cb04e8f21efc0ab65f29ae7d313c1ebb7
etrod <157399562+etrod@users.noreply.github.com>
2024-05-01 11:38:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -74,11 +74,6 @@ document.getElementById('clickButton').addEventListener('click', function() {
     playClickSound();
 });
 
-document.getElementById('autoClicker').addEventListener('click', buyAutoClicker);
-document.getElementById('megaClicker').addEventListener('click', buyMegaClicker);
-document.getElementById('doubleClicks').addEventListener('click', buyDoubleClicks);
-document.getElementById('bonusClicks').addEventListener('click', buyBonusClicks);
-
 document.addEventListener("DOMContentLoaded", function(event) {
     document.getElementById("shop").style.display = "block";
 });
